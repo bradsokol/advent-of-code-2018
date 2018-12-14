1ec273001e373e88b545751bdca8dee0a117da4c
Brad Sokol <bsokol@gmail.com>
2018-12-14 09:31:43 -0500

    First attempt at Day 14 part 2

--- day14.rb
+++ day14.rb
@@ -6,24 +6,33 @@ require 'pp'
 require 'pry'
 
 def iterate(recipes, elf1, elf2)
-  score1 = recipes[elf1]
-  score2 = recipes[elf2]
+  score1 = recipes[elf1].to_i
+  score2 = recipes[elf2].to_i
   sum = score1 + score2
   if sum > 9
-    recipes += [sum / 10, sum % 10]
+    recipes += "#{sum / 10}#{sum % 10}"
   else
-    recipes << sum
+    recipes += "#{sum}"
   end
   [recipes, (elf1 + score1 + 1) % recipes.length, (elf2 + score2 + 1) % recipes.length]
 end
 
-input = 260_321
-recipes = [3, 7]
+input = '260321'
+recipes = '37'
 elf1 = 0
 elf2 = 1
 
-until recipes.length >= input + 10
-  recipes, elf1, elf2 = iterate(recipes, elf1, elf2)
+while recipes.index(input).nil?
+  score1 = recipes[elf1].to_i
+  score2 = recipes[elf2].to_i
+  sum = score1 + score2
+  if sum > 9
+    recipes += "#{sum / 10}#{sum % 10}"
+  else
+    recipes += "#{sum}"
+  end
+  elf1 = (elf1 + score1 + 1) % recipes.length
+  elf2 = (elf2 + score2 + 1) % recipes.length
 end
 
-pp recipes[input...(input + 10)].join
+pp recipes.index(input)
