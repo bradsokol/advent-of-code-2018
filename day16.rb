4f5874a7248c07f0122e4d86ad1eeb1c2324f86c
Brad Sokol <bsokol@gmail.com>
2018-12-18 11:09:12 -0500

    Day 16 part 2

--- day16.rb
+++ day16.rb
@@ -4,6 +4,22 @@
 
 require 'pp'
 require 'pry'
+require 'set'
+
+def print_matches
+  puts ''
+  printf '   '
+  16.times { |i| printf(" %02i ", i) }
+  puts ''
+  puts '-' * 70
+  $matches.each_with_index do |x, i|
+    printf("%02i|", i)
+    x.each do |y|
+      printf("%3i ", y)
+    end
+    puts ''
+  end
+end
 
 $registers = Array.new(4, 0)
 $counts = Array.new(16, 0)
@@ -34,6 +50,8 @@ seti = lambda { |a, _, c| $registers[c] = a }
 $ops=[addi, addr, bani, banr, bori, borr, eqir, eqri, eqrr, gtir, gtri, gtrr, muli, mulr, seti, setr]
 input = File.open('day16.txt')
 
+$matches = []
+16.times { |i| $matches[i] = Array.new(16, 0) }
 774.times do |n|
   before = input.readline.match(/Before: \[(\d+), (\d+), (\d+), (\d+)\]/).captures.map(&:to_i)
   op_code = input.readline.match(/^(\d+) (\d+) (\d+) (\d+)/).captures.map(&:to_i)
@@ -41,12 +59,35 @@ input = File.open('day16.txt')
   input.readline
 
   count = 0
-  16.times do |i|
+  16.times do |mnemonic|
     $registers = before.clone
-    $ops[i].call(op_code[1], op_code[2], op_code[3])
-    count += 1 if $registers == after
+    $ops[mnemonic].call(op_code[1], op_code[2], op_code[3])
+    if $registers == after
+      count += 1
+      $matches[mnemonic][op_code[0]] += 1
+    end
   end
   $counts[count] += 1
 end
 
 puts $counts[3..-1].sum
+
+$ops_map = Hash.new(-1)
+until $ops_map.length == 16
+  one = $matches.find { |r| r.count(0) == 15 }
+  mnemonic = $matches.index(one)
+  op_code = one.index { |o| o != 0 }
+  $ops_map[op_code] = mnemonic
+
+  $matches.each { |r| r[op_code] = 0 }
+end
+
+$registers = Array.new(4, 0)
+
+input.readline
+input.readline
+input.readlines.each do |line|
+  op_code, a, b, c = line.match(/^(\d+) (\d+) (\d+) (\d+)/).captures.map(&:to_i)
+  $ops[$ops_map[op_code]].call(a, b, c)
+end
+puts $registers[0]
