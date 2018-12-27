2bb84d0c618cf798c208785f80c919b912ca8fe5
Brad Sokol <bsokol@gmail.com>
2018-12-27 09:16:30 -0500

    Day 20

--- day20.rb
+++ day20.rb
@@ -3,124 +3,47 @@
 require 'pry'
 require 'set'
 
-class Room
-  attr_accessor :north, :east, :south, :west, :x, :y
+$data = File.open('day20.txt').readline
+$data = '^WNE$'
+$data = '^ENWWW(NEEE|SSE(EE|N))$'
+$data = '^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$'
 
-  def initialize(x, y)
-    @x = x
-    @y = y
-    @north = @east = @south = @west = '?'
-  end
+$directions = {
+  'N' =>  Complex(0, 1),
+  'S' =>  Complex(0, -1),
+  'E' =>  1,
+  'W' =>  -1,
+}
+$map = { 0 => 0 }
 
-  def visualize
-    puts "\##{@north == '?' ? '?' : '-'}\#"
-    puts "#{@west == '?' ? '?' : '|'}.#{@east == '?' ? '?' : '|'}"
-    puts "\##{@south == '?' ? '?' : '-'}\#"
-  end
+def traverse(enum, pos=0, depth=0)
+  initial_pos = pos
+  initial_depth = depth
 
-  def adjacent
-    [north, east, west, south].delete_if { |r| r == '?' }
-  end
-end
-
-def add_room(x, y, from_room, direction)
-  room = $rooms[y][x]
-  unless room
-    room = $rooms[y][x] = Room.new(x, y)
-  end
-
-  case direction
-  when 'N'
-    from_room.north = room
-    room.south = from_room
-  when 'E'
-    from_room.east = room
-    room.west = from_room
-  when 'S'
-    from_room.south = room
-    room.north = from_room
-  when 'W'
-    from_room.west = room
-    room.east = from_room
-  end
-
-  room
-end
-
-$rooms = Hash.new{ |hash, key| hash[key] = Hash.new }
-current_room = Room.new(0, 0)
-my_location = current_room
-x = y = 0
-$rooms[y][x] = current_room
-
-regex = File.open('day20.txt').readline
-# regex = '^WNE$'
-# regex = '^ENWWW(NEEE|SSE(EE|N))$'
-# regex = '^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$'
-# regex = '^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$'
-i = 1
-forks = []
-continuations = []
-dollars = 0
-loop do
-  char = regex[i]
-  if char == '$'
-    dollars += 1
-    puts "#{dollars} #{continuations.length}"
-
-    break if continuations.empty?
-    current_room, i, forks = continuations.pop
-    x = 1
-    until x == 0
-      x -= 1 if regex[i] == ')'
-      x += 1 if regex[i] == '('
-      i += 1
+  enum.each do |char|
+    if $directions.key?(char)
+      pos += $directions[char]
+      if $map.key?(pos)
+        depth = $map[pos]
+      else
+        depth += 1
+        $map[pos] = depth
+      end
+    elsif char == '|'
+      pos = initial_pos
+      depth = initial_depth
+    elsif char == '('
+      traverse(enum, pos, depth)
+    elsif char == ')'
+      return
+    elsif char == '$'
+      return
     end
-    char = regex[i]
   end
-
-  case char
-  when '$'
-    next
-  when 'N'
-    y += 1
-    current_room = add_room(x, y, current_room, char)
-  when 'E'
-    x += 1
-    current_room = add_room(x, y, current_room, char)
-  when 'S'
-    y -= 1
-    current_room = add_room(x, y, current_room, char)
-  when 'W'
-    x -= 1
-    current_room = add_room(x, y, current_room, char)
-  when '('
-    forks.push(current_room)
-  when ')'
-    forks.pop
-    continuations.pop if regex[i - 1] == '|'
-  when '|'
-    continuations.push([current_room, i, forks.clone])
-    current_room = forks.last
-    x = current_room.x
-    y = current_room.y
-  end
-  i += 1
 end
 
-def find_path(current_room, distance)
-  next_rooms = current_room.adjacent.delete_if { |r| $visited.include?(r) }
-  $visited.merge(next_rooms)
-  $longest_path = [$longest_path, distance].max
-  $one_thousand += 1 if distance == 1000
-  $one_thousand += next_rooms.length if distance > 1000
-  next_rooms.each { |r| find_path(r, distance + 1) }
-end
-
-$longest_path = 0
-$one_thousand = 0
-$visited = Set.new
-find_path(my_location, 0)
-
-p $longest_path
-p $one_thousand
+traverse($data.chars[1..-1].each)
+puts $map.values.max
+puts $map.values
+  .select { |v| v >= 1000 }
+  .sum
